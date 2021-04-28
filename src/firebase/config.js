3826540c9bc0da9d77e035dd49ae24d1bce86ff5
jagadishpatil <jagadishpatil231@gmail.com>
2021-04-28 14:21:03 +0530

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+//import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCx_yRjPmI7CWld87TiSjQ7I9BtjkHPjgQ",
+    authDomain: "jp-firegram.firebaseapp.com",
+    projectId: "jp-firegram",
+    storageBucket: "jp-firegram.appspot.com",
+    messagingSenderId: "67308421784",
+    appId: "1:67308421784:web:fa876dedf01cd8b89bc6ed"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+  const timestamp = firebase.firestore.FieldValue.serverTimeStamp;
+
+  export { projectStorage, projectFirestore, timestamp };
